ae1d7864470aef0e2ed0c5d5808bc6cae2cce0ad
mokkabca <95523018+mokkabca@users.noreply.github.com>
2022-08-24 15:18:34 +0530

    Update packages.sh

--- packages.sh
+++ packages.sh
@@ -2,15 +2,15 @@
 
 
 #
-#  Copyright (c) 2022 Barchampas Gerasimos <makindosxx@gmail.com>.
-#  mip22 is a advanced phishing tool.
+#  Copyright (c) 2022 Barchampas Gerasimos .
+#  thc is a advanced phishing tool.
 #
-#  mip22 is free software: you can redistribute it and/or modify
+#  thc is free software: you can redistribute it and/or modify
 #  it under the terms of the GNU Affero General Public License as published by
 #  the Free Software Foundation, either version 3 of the License, or
 #  (at your option) any later version.
 #
-#  mip22 is distributed in the hope that it will be useful,
+#  thc is distributed in the hope that it will be useful,
 #  but WITHOUT ANY WARRANTY; without even the implied warranty of
 #  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 #  GNU Affero General Public License for more details.
